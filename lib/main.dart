05c633cb1c1b54b66aee5ef1b8a3aa80571e5314
jaydhurve09 <jaydhurve09@gmail.com>
2025-06-02 13:00:11 +0530

    Button Created

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,19 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My App')),
-        body: Center(child: Text('Hello World!')),
+        body: Center(
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              minimumSize: Size(280, 80),
+              textStyle: TextStyle(fontSize: 30),
+              backgroundColor: Colors.amber,
+              foregroundColor: Colors.black,
+              side: BorderSide(color: Colors.red, width: 3),
+            ),
+            child: Text('Outlined Button'),
+            onPressed: () => {print('Outlined Button Pressed!')},
+          ),
+        ),
       ),
     ),
   );
